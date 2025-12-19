5716786267beaa9fbcea94c6474b3370ebec1973
4drea4 <bakerand@outlook.com>
2025-12-19 16:29:43 -0600

    all blog posts

--- src/pages/blogs/AllBlogPosts.tsx
+++ src/pages/blogs/AllBlogPosts.tsx
@@ -1,2 +1,14 @@
 import { Link } from "react-router-dom";
 import { posts } from "../../models/posts";
+
+export default function AllBlogPosts(){
+    return (
+        <div className="allBlogsDiv">
+            <h2>Blog</h2>
+            {posts.map((post) => (
+                
+            )
+        )}
+        </div>
+    )
+}
