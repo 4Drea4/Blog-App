ed03438a6f4401d8a57750ee2491d67903a1be66
4drea4 <bakerand@outlook.com>
2025-12-19 15:46:59 -0600

    imports in main and making modeled data

--- src/models/posts.ts
+++ src/models/posts.ts
@@ -5,10 +5,20 @@ export const posts: BlogPost[] = [
 id: 150,
 slug: "writing-changes-lives",
 title: "The Power of Writing",
-content: ""
+content: "This is a sample of writing content"
+},
+{
+id: 15,
+slug: "Why trends repeat themselves",
+title: "Circle of trend",
+content: "This is a sample ofwriting content about new content"
+ },
+ {
+   id: 109,
+   slug: "bubbles-are-bubbly",
+    title: "Pop!",
+    content: "Why do bubblescaptivate the hearts of everyoneof all ages"
 },
 
-
-
-
 ]
+export function getPostSlug(slug: string)
