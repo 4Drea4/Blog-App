7d5e07a07372cf6cfca70382cf89af7b4899f375
4drea4 <bakerand@outlook.com>
2025-12-19 15:27:09 -0600

    started model data

---
+++ src/models/posts.ts
@@ -0,0 +1,14 @@
+import type { BlogPost } from "../types";
+
+export const posts: BlogPost[] = [
+{
+id: 150,
+slug: "writing-changes-lives",
+title: "The Power of Writing",
+content: ""
+},
+
+
+
+
+]
