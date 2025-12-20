47204e96af1cdf9e648401d7a2cea64eaf9ec3ee
4drea4 <bakerand@outlook.com>
2025-12-19 21:44:58 -0600

    setting up app

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,26 @@
-import { useState } from 'react'
+import { Routes, Route, Navigate} from 'react-router-dom';
+import BlogPostPage from './pages/blogs/IndividualBlog';
+import AllBlogPosts from './pages/blogs/AllBlogPosts';
+// import Home from './pages/home/Home';
+// import Admin from './pages/admin/Admin';
+// import Login from './pages/login/Login'
+// import NotFound from './pages/NotFound/NotFound'
+
 import './App.css'
 
-function App() {
-
+export default function App() {
 
   return (
-    <>
-      
-     
-    </>
+    <Routes>
+      <Route path='/' element={<Home/>}/>
+      <Route path='/blog' element={<AllBlogPosts/>}/>
+      <Route path='/admin' element={<Admin/>}/>
+      <Route path='/blog/:slug' element={<IndividualBlog/>}/>
+      <Route path="/login" element={<Login/>} />
+      <Route path="*" element={<NotFound/>} />
+   
+
+    </Routes >
   )
 }
 
-export default App
