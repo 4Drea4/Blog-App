9372bd5b94acc0ebaa555c602ced0fdfbe74ba0e
4drea4 <bakerand@outlook.com>
2025-12-20 18:33:59 -0600

    wrote auth context

--- src/contexts/AuthContext.tsx
+++ src/contexts/AuthContext.tsx
@@ -1,5 +1,23 @@
-import { createContext, useContext } from "react";
+import React, { createContext, useContext, useState } from "react";
 import type { AuthContextType } from "../types";
 
 const AuthContext = createContext<AuthContextType | null>(null);
+
+export function AuthProvider({children}: {children : React.ReactNode}) {
+    const [isAuthenticated, setIsAuthenticated] = useState(false);
+
+    function login(){
+        setIsAuthenticated(true);
+    }
+
+    function logout(){
+        setIsAuthenticated(false);
+
+    }
+    return (
+        <AuthContext.Provider value = {{ isAuthenticated , login , logout}}>
+            {children}
+        </AuthContext.Provider>
+    )
+}
 
